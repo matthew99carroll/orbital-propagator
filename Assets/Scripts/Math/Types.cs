8a44d7adfe7fc1080c61f501331520e8dabee879
matthew99carroll <matthew99carroll@gmail.com>
2021-11-03 22:35:36 +1300

    Added two-impulsive rendezvous maneuver

--- Assets/Scripts/Math/Types.cs
+++ Assets/Scripts/Math/Types.cs
@@ -44,6 +44,13 @@ namespace Math
                 Y = y;
                 Z = z;
             }
+
+            public Vector3D()
+            {
+                X = 0;
+                Y = 0;
+                Z = 0;
+            }
         }
         
         /*
@@ -81,6 +88,21 @@ namespace Math
                 ZY = zy;
                 ZZ = zz;
             }
+
+            public Matrix3x3()
+            {
+                XX = 0;
+                XY = 0;
+                XZ = 0;
+
+                YX = 0;
+                YY = 0;
+                YZ = 0;
+
+                ZX = 0;
+                ZY = 0;
+                ZZ = 0;
+            }
         }
         
         /*
@@ -112,6 +134,30 @@ namespace Math
             {
                 V1 = v1;
                 V2 = v2;
+            }
+        }
+
+        public class CloshessyWiltshireMatrices
+        {
+            public Matrix3x3 phirr;
+            public Matrix3x3 phirv;
+            public Matrix3x3 phivr;
+            public Matrix3x3 phivv;
+
+            public CloshessyWiltshireMatrices(Matrix3x3 _phirr, Matrix3x3 _phirv, Matrix3x3 _phivr, Matrix3x3 _phivv)
+            {
+                phirr = _phirr;
+                phirv = _phirv;
+                phivr = _phivr;
+                phivv = _phivv;
+            }
+
+            public CloshessyWiltshireMatrices()
+            {
+                phirr = new Matrix3x3();
+                phirv = new Matrix3x3();
+                phivr = new Matrix3x3();
+                phivv = new Matrix3x3(); 
             }
         }
         
@@ -185,6 +231,93 @@ namespace Math
         public static Vector3D SubtractVectors(Vector3D vectorA, Vector3D vectorB)
         {
             return new Vector3D(vectorA.X - vectorB.X, vectorA.Y - vectorB.Y, vectorA.Z - vectorB.Z);
+        }
+
+        public static double CalculateDeterminant3x3(Matrix3x3 matrix)
+        {
+            return (matrix.XX * (matrix.YY * matrix.ZZ - matrix.YZ * matrix.ZY)) -
+                   (matrix.XY * (matrix.YX * matrix.ZZ - matrix.YZ * matrix.ZX)) +
+                   (matrix.XZ * (matrix.YX * matrix.ZY - matrix.YY * matrix.ZX));
+        }
+
+        public static Matrix3x3 ScalarMultipliedMatrix(double scalar, Matrix3x3 matrix)
+        {
+            Matrix3x3 matrixC = new Matrix3x3();
+
+            matrixC.XX = scalar * matrix.XX;
+            matrixC.XY = scalar * matrix.XY;
+            matrixC.XZ = scalar * matrix.XZ;
+
+            matrixC.YX = scalar * matrix.YX;
+            matrixC.YY = scalar * matrix.YY;
+            matrixC.YZ = scalar * matrix.YZ;
+
+            matrixC.ZX = scalar * matrix.ZX;
+            matrixC.ZY = scalar * matrix.ZY;
+            matrixC.ZZ = scalar * matrix.ZZ;
+
+            return matrixC;
+        }
+
+        public static Matrix3x3 CalculateInverseMatrix(Matrix3x3 matrix)
+        {
+            double determinant = CalculateDeterminant3x3(matrix);
+
+            Matrix3x3 matrixInverse = new Matrix3x3();
+
+            matrixInverse.XX = matrix.YY * matrix.ZZ - matrix.YZ * matrix.ZY;
+            matrixInverse.XY = matrix.YZ * matrix.ZX - matrix.YX * matrix.ZZ;
+            matrixInverse.XZ = matrix.YX * matrix.ZY - matrix.YY * matrix.ZX;
+
+            matrixInverse.YX = matrix.ZY * matrix.XZ - matrix.ZX * matrix.XY;
+            matrixInverse.YY = matrix.XX * matrix.ZZ - matrix.XZ * matrix.ZX;
+            matrixInverse.YZ = matrix.XZ * matrix.XY - matrix.XX * matrix.YZ;
+
+            matrixInverse.ZX = matrix.YX * matrix.ZY - matrix.YZ * matrix.YX;
+            matrixInverse.ZY = matrix.YZ * matrix.ZX - matrix.YX * matrix.ZY;
+            matrixInverse.ZZ = matrix.YX * matrix.XY - matrix.YY * matrix.ZX;
+
+            matrixInverse = ScalarMultipliedMatrix(1 / determinant, matrixInverse);
+
+            return matrixInverse;
+        }
+
+        public static Matrix3x3 MatrixMultiply(Matrix3x3 matrixA, Matrix3x3 matrixB)
+        {
+            Matrix3x3 matrixC = new Matrix3x3();
+
+            matrixC.XX = matrixA.XX * matrixB.XX + matrixA.XY * matrixB.YX + matrixA.XZ * matrixB.ZX;
+            matrixC.XY = matrixA.XX * matrixB.XY + matrixA.XY * matrixB.YY + matrixA.XZ * matrixB.ZY;
+            matrixC.XZ = matrixA.XX * matrixB.XZ + matrixA.XY * matrixB.YZ + matrixA.XZ * matrixB.ZZ;
+
+            matrixC.YX = matrixA.YX * matrixB.XX + matrixA.YY * matrixB.YX + matrixA.YZ * matrixB.ZX;
+            matrixC.YY = matrixA.YX * matrixB.XY + matrixA.YY * matrixB.YY + matrixA.YZ * matrixB.ZY;
+            matrixC.YZ = matrixA.YX * matrixB.XZ + matrixA.YY * matrixB.YZ + matrixA.YZ * matrixB.ZZ;
+
+            matrixC.ZX = matrixA.ZX * matrixB.XX + matrixA.ZY * matrixB.YX + matrixA.ZZ * matrixB.ZX;
+            matrixC.ZY = matrixA.ZX * matrixB.XY + matrixA.ZY * matrixB.YY + matrixA.ZZ * matrixB.ZY;
+            matrixC.ZZ = matrixA.ZX * matrixB.XZ + matrixA.ZY * matrixB.YZ + matrixA.ZZ * matrixB.ZZ;
+
+            return matrixC;
+        }
+
+        public static Matrix3x3 MatrixAddition(Matrix3x3 matrixA, Matrix3x3 matrixB)
+        {
+            Matrix3x3 matrixC = new Matrix3x3();
+
+            matrixC.XX = matrixA.XX + matrixB.XX;
+            matrixC.XY = matrixA.XY + matrixB.XY;
+            matrixC.XZ = matrixA.XZ + matrixB.XZ;
+
+            matrixC.YX = matrixA.YX + matrixB.YX;
+            matrixC.YY = matrixA.YY + matrixB.YY;
+            matrixC.YZ = matrixA.YZ + matrixB.YZ;
+
+            matrixC.ZX = matrixA.ZX + matrixB.ZX;
+            matrixC.ZY = matrixA.ZY + matrixB.ZY;
+            matrixC.ZZ = matrixA.ZZ + matrixB.ZZ;
+
+            return matrixC;
         }
     }
 }
---
+++ Assets/Scripts/Physics/OrbitManeuvers/TwoImpulseRendezvousManeuver.cs
@@ -0,0 +1,150 @@
+﻿using Math;
+using static Math.Types;
+using static System.Math;
+
+namespace Physics.OrbitManeuvers
+{
+    public static class TwoImpulseRendezvousManeuver
+    {
+        private static Vector3D _directionCosineMatrixGefToChaserI,
+                                _directionCosineMatrixGefToChaserJ,
+                                _directionCosineMatrixGefToChaserK;
+
+        private static Matrix3x3 _directionCosineMatrix;
+        private static Vector3D _relPositionChaserToTargetGef;
+        private static double _meanMotionTarget;
+        private static Vector3D _angularVelocityTarget;
+        private static Vector3D _relVelocityChaserToTargetGef;
+
+        private static Vector3D _initRelPositionTargetsRefFrame, _initRelVelocityTargetsRefFrameMinus;
+        private static Vector3D _initRelVelocityTargetsRefFramePlus;
+        private static Vector3D _finalRelVelocityTargetsRefFrameMinus;
+
+        private static CloshessyWiltshireMatrices _matrices;
+        
+        private static Vector3D _initialDeltaV, _finalDeltaV;
+        private static double _deltaV;
+        private static double _propellantMassExpended;
+        
+        public static ManeuverRequirements CalculateRendezvousManeuver(
+            Vector3D positionGeoEqChaser,
+            Vector3D velocityGeoEqChaser,
+            Vector3D positionGeoEqTarget,
+            Vector3D velocityGeoEqTarget,
+            double rendezvousTime,
+            double chaserMass,
+            double chaserPropellantIsp)
+        {
+            _directionCosineMatrixGefToChaserI = new Vector3D();
+            _directionCosineMatrixGefToChaserJ = new Vector3D();
+            
+            // Calculate row 1, column i, j, k of the direction cosine matrix
+            _directionCosineMatrixGefToChaserI.X =
+                positionGeoEqTarget.X / CalculateVectorNorm3D(positionGeoEqTarget);
+            _directionCosineMatrixGefToChaserI.Y =
+                positionGeoEqTarget.Y / CalculateVectorNorm3D(positionGeoEqTarget);
+            _directionCosineMatrixGefToChaserI.Z =
+                positionGeoEqTarget.Z / CalculateVectorNorm3D(positionGeoEqTarget);
+
+            // Calculate row 2, column i, j, k of the direction cosine matrix
+            _directionCosineMatrixGefToChaserJ.X =
+                velocityGeoEqTarget.X / CalculateVectorNorm3D(velocityGeoEqTarget);
+            _directionCosineMatrixGefToChaserJ.Y =
+                velocityGeoEqTarget.Y / CalculateVectorNorm3D(velocityGeoEqTarget);
+            _directionCosineMatrixGefToChaserJ.Z =
+                velocityGeoEqTarget.Z / CalculateVectorNorm3D(velocityGeoEqTarget);
+
+            // Calculate row 3 of the direction cosine matrix by getting the cross product of row 1 and 2
+            _directionCosineMatrixGefToChaserK = CalculateCrossProduct3D(_directionCosineMatrixGefToChaserI,
+                _directionCosineMatrixGefToChaserJ);
+
+            _directionCosineMatrix = new Matrix3x3();
+            
+            // Assign the three vectors calculated above into the 3x3 matrix
+            _directionCosineMatrix.XX = _directionCosineMatrixGefToChaserI.X;
+            _directionCosineMatrix.XY = _directionCosineMatrixGefToChaserI.Y;
+            _directionCosineMatrix.XZ = _directionCosineMatrixGefToChaserI.Z;
+            _directionCosineMatrix.YX = _directionCosineMatrixGefToChaserJ.X;
+            _directionCosineMatrix.YY = _directionCosineMatrixGefToChaserJ.Y;
+            _directionCosineMatrix.YZ = _directionCosineMatrixGefToChaserJ.Z;
+            _directionCosineMatrix.ZX = _directionCosineMatrixGefToChaserK.X;
+            _directionCosineMatrix.ZY = _directionCosineMatrixGefToChaserK.Y;
+            _directionCosineMatrix.ZZ = _directionCosineMatrixGefToChaserK.Z;
+
+            // Calculate the relative position of the chase vehicle to the target in the Geocentric Equatorial Frame of reference
+            _relPositionChaserToTargetGef = SubtractVectors(positionGeoEqChaser, positionGeoEqTarget);
+
+            // Calculate the mean motion of the target, which is used to calculate the angular velocity of the target
+            _meanMotionTarget = CalculateVectorNorm3D(velocityGeoEqTarget) / CalculateVectorNorm3D(positionGeoEqTarget);
+
+            // Angular velocity of target
+            _angularVelocityTarget = new Vector3D(0, 0, _meanMotionTarget);
+
+            // Calculate the relative velocity of the chase vehicle to the target in the Geocentric Equatorial Frame of reference
+            _relVelocityChaserToTargetGef = SubtractVectors(SubtractVectors(velocityGeoEqChaser, velocityGeoEqTarget),
+                CalculateCrossProduct3D(_angularVelocityTarget,
+                    _relPositionChaserToTargetGef));
+
+            // Calculate the initial position vector at the beginning of the rendezvous in the targets frame of reference
+            _initRelPositionTargetsRefFrame =
+                MatrixVectorProduct3D(_directionCosineMatrix, _relPositionChaserToTargetGef);
+
+            // Calculate the initial velocity vector just before launch into the rendezvous trajectory
+            _initRelVelocityTargetsRefFrameMinus =
+                MatrixVectorProduct3D(_directionCosineMatrix, _relVelocityChaserToTargetGef);
+            
+            // Initialise empty Closhessy-Wiltshire matrix datatype
+            _matrices = new CloshessyWiltshireMatrices();
+            
+            // Closhessy-Wiltshire Matrix Phi rr
+            _matrices.phirr = new Matrix3x3((4 - 3 * Cos(_meanMotionTarget * rendezvousTime)), 0.0, 0.0,
+                (6 * (Sin(_meanMotionTarget * rendezvousTime) - _meanMotionTarget * rendezvousTime)), 1.0, 0.0,
+                0.0, 0.0, Cos(_meanMotionTarget * rendezvousTime));
+            
+            // Closhessy-Wiltshire Matrix Phi rv
+            _matrices.phirv = new Matrix3x3((Sin(_meanMotionTarget * rendezvousTime) / _meanMotionTarget),
+                ((2 * (1 - Cos(_meanMotionTarget * rendezvousTime))) / _meanMotionTarget), 0.0,
+                ((2 * (Cos(_meanMotionTarget * rendezvousTime) - 1)) / _meanMotionTarget),
+                ((4 * Sin(_meanMotionTarget * rendezvousTime) - 3 * _meanMotionTarget * rendezvousTime) /
+                 _meanMotionTarget), 0.0,
+                0.0, 0.0, Sin(_meanMotionTarget * rendezvousTime) / _meanMotionTarget);
+            
+            // Closhessy-Wiltshire Matrix Phi vr
+            _matrices.phivr = new Matrix3x3((3 * _meanMotionTarget * Sin(_meanMotionTarget * rendezvousTime)), 0.0, 0.0,
+                (6 * _meanMotionTarget * (Cos(_meanMotionTarget * rendezvousTime) - 1.0)), 0.0, 0.0,
+                0.0, 0.0, -_meanMotionTarget * Sin(_meanMotionTarget * rendezvousTime));
+            
+            // Closhessy-Wiltshire Matrix Phi vv
+            _matrices.phivv = new Matrix3x3(Cos(_meanMotionTarget * rendezvousTime),
+                2 * Sin(_meanMotionTarget * rendezvousTime), 0.0,
+                -2 * Sin(_meanMotionTarget * rendezvousTime), 4 * Cos(_meanMotionTarget * rendezvousTime) - 3, 0.0,
+                0.0, 0.0, Cos(_meanMotionTarget * rendezvousTime));
+            
+            // Calculate the initial relative velocity just inside the rendezvous window in the targets frame of reference
+            _initRelVelocityTargetsRefFramePlus = MatrixVectorProduct3D(
+                MatrixMultiply(ScalarMultipliedMatrix(-1, CalculateInverseMatrix(_matrices.phirv)), _matrices.phirr),
+                _initRelPositionTargetsRefFrame);
+            
+            // Calculate the final relative velocity just inside the rendezvous window in the targets frame of reference
+            _finalRelVelocityTargetsRefFrameMinus = AddVectors(
+                MatrixVectorProduct3D(_matrices.phivr, _initRelPositionTargetsRefFrame),
+                MatrixVectorProduct3D(_matrices.phivv, _initRelVelocityTargetsRefFramePlus));
+            
+            // Calculate the initial delta V at the start of the rendezvous window
+            _initialDeltaV =
+                SubtractVectors(_initRelVelocityTargetsRefFramePlus, _initRelVelocityTargetsRefFrameMinus);
+            
+            // Calculate the final delta V at the end of the rendezvous window
+            _finalDeltaV = SubtractVectors(new Vector3D(0, 0, 0), _finalRelVelocityTargetsRefFrameMinus);
+            
+            // Calulate the delta V used in the maneuver
+            _deltaV = Abs(CalculateVectorNorm3D(_initialDeltaV)) + Abs(CalculateVectorNorm3D(_finalDeltaV));
+            
+            // Calculate the propellant mass expended doing the maneuver
+            _propellantMassExpended = (1 - Exp(-_deltaV / (chaserPropellantIsp * Constants.EARTH_GRAVITY_0))) *
+                                      chaserMass;
+
+            return new ManeuverRequirements(_deltaV, _propellantMassExpended);
+        }
+    }
+}
